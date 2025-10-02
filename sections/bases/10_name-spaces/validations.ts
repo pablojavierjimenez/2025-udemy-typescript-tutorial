0421b515ef2b547455348124970b253a59fe9719
Pablo Jimenez <pablojavierjimenez@gmail.com>
2025-10-02 10:58:17 -0300

    docs: :memo: creando mi primer namespace
    en este capitulo de la decima clase se mostro el tema de los name spaces y como puede utilizarse para encapsular y asi evitar coliciones de nombres en nuestros programas

---
+++ sections/bases/10_name-spaces/validations.ts
@@ -0,0 +1,59 @@
+/*
+  ¿Qué es un namespace en TypeScript?
+
+  - Un namespace es una forma de organizar y agrupar código dentro de un mismo archivo o 
+    a través de múltiples archivos.
+
+  - Su objetivo principal es evitar colisiones de nombres (por ejemplo, dos clases o funciones 
+    que se llamen igual en un mismo proyecto).
+
+  - Los miembros de un namespace (clases, funciones, interfaces, constantes, etc.) 
+    deben ser exportados explícitamente con `export` para poder ser usados fuera del namespace.
+
+  - Para acceder a algo dentro de un namespace, usamos la notación de punto.
+    Ejemplo: `MiEspacioDeNombres.MiClase`.
+
+  - Hoy en día se recomienda usar **módulos de ES6 (import/export)** en lugar de namespaces,
+    ya que los módulos tienen mejor soporte en proyectos grandes y modernos.
+*/
+
+
+// Ejemplo de uso de namespace en TypeScript:
+namespace Geometry {
+  export class Circulo {
+    constructor(public radio: number) {}
+    area() {
+      return Math.PI * this.radio * this.radio;
+    }
+  }
+
+  export class Square {
+    constructor(public base: number, public altura: number) {}
+    area() {
+      return this.base * this.altura;
+    }
+  }
+}
+
+// Accediendo a las clases del namespace:
+const c = new Geometry.Circulo(5);
+const r = new Geometry.Square(10, 4);
+
+console.log("Área del círculo:", c.area());
+console.log("Área del rectángulo:", r.area());
+
+/**
+  * ¿O tambien se puede utilizar para encapsular funciones sueltas
+*/
+namespace Validations {
+  export const validateText = (text: string): boolean => {
+    return text.length >= 3;
+  };
+
+  export const validateDate = (myDate: Date): boolean => {
+    return isNaN(myDate.valueOf()) ? true : false;
+  };
+
+}
+
+console.log(Validations.validateText('Hola'));
--- sections/src/main.js
+++ sections/src/main.js
@@ -1,106 +1,12 @@
 "use strict";
-(() => {
-    console.log("// ------------- INTERFACES 1 BASICS ------------- //");
-    ;
-})();
-(() => {
-    console.log("// ------------- INTERFACES 2 BASICS ------------- //");
-    ;
-    const cliente = {
-        age: 10,
-        name: 'Juan',
-        address: {
-            zip: 12345,
-            id: 'MDQ',
-            city: "Monaco"
-        },
-        getFullAddress(id) {
-            var _a;
-            console.log(id, (_a = this.address) === null || _a === void 0 ? void 0 : _a.city);
-        },
+var Validations;
+(function (Validations) {
+    Validations.validateText = (text) => {
+        return text.length >= 3;
     };
-})();
-(() => {
-    console.log("// ------------- INTERFACES 3 CLASSES and INTERFACES ------------- //");
-    class Hero {
-        constructor() {
-            this.name = '';
-            this.realName = '';
-        }
-    }
-    class Xmen extends Hero {
-        constructor() {
-            super(...arguments);
-            this.name = '';
-            this.realName = '';
-            this.age = 20;
-        }
-    }
-    class Villains extends Hero {
-        constructor() {
-            super(...arguments);
-            this.name = '';
-            this.realName = '';
-            this.isAlien = false;
-        }
-        mutantPower(id) {
-            return 'esta function debe ser sobre escrita cuando se implemente en un objeto';
-        }
-    }
-    const gambit = new Xmen();
-    gambit.name = 'Gambito';
-    gambit.age = 34;
-    const galaxia = new Villains();
-    galaxia.isAlien = true;
-    galaxia.name = 'Galactus';
-    console.log(gambit, galaxia);
-})();
-(() => {
-    console.log("// ------------- INTERFACES 4 FUNCTIONS ------------- //");
-    ;
-    let addTwoNumbersFunction;
-    addTwoNumbersFunction = (a, b) => {
-        return a + b;
+    Validations.validateDate = (myDate) => {
+        return isNaN(myDate.valueOf()) ? true : false;
     };
-})();
-const conducirBatMovil = (auto) => {
-    auto.encender = true;
-    auto.velocidadMaxima = 100;
-    auto.acelerar();
-};
-const batMovil = {
-    encender: false,
-    velocidadMaxima: 0,
-    acelerar() {
-        console.log("...... go go go!!!");
-    },
-};
-const joker = {
-    cantar: true,
-    comer: true,
-    llorar: false,
-};
-const cantar = (joker) => {
-    if (joker.cantar) {
-        console.log("JAJAJAJA");
-    }
-};
-const batCity = (ciudadanos) => {
-    return ciudadanos.length;
-};
-class Persona {
-    constructor() {
-        this.nombre = '';
-        this.edad = 0;
-        this.sexo = 'O';
-        this.estadoCivil = '';
-    }
-    imprimirBio() {
-        console.log(`esta es la bio de : -> ${this.nombre}`);
-    }
-}
-const juan = new Persona();
-juan.nombre = 'Juan';
-juan.sexo = 'M';
-console.log(juan, juan.imprimirBio());
+})(Validations || (Validations = {}));
+console.log(Validations.validateText('Hola'));
 //# sourceMappingURL=main.js.map
