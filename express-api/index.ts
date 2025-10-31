d11eba3787abfa1e431098faf24ed05da5cecc1c
Pablo Jimenez <pablojavierjimenez@gmail.com>
2025-10-31 06:53:17 -0300

    feat: add initial settings for cSpell and update error messages in API routes

--- express-api/index.ts
+++ express-api/index.ts
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
 
   res.status(401).json({
     result: 'error',
-    msg:'cambiaste'
+    msg:'prueva con la ruta /api/{cualquier-texto-o-numero}'
   })
   // res.json({
   //   result: 'ok',
@@ -16,6 +16,29 @@ app.get('/', (req, res) => {
   // })
 })
 
+app.get('/api', (req, res) => {
+    res.status(401).json({
+      result: 'error',
+      msg:`debes enviar algún parámetro luego de api, ejemplo: /api/hello-world`
+    })
+})
+
+/**
+ * Endpoint que recibe un parámetro por la URL y lo devuelve en la respuesta
+ * @param text parámetro recibido por la URL
+ * @return json con el parámetro recibido
+ * ejemplo: /api/hello-world
+ * respuesta: { result: 'ok', msg: 'Funciona!! recibí --> hello-world <-- por URL' }
+ * ejemplo: /api/12345
+ * respuesta: { result: 'ok', msg: 'Funciona!! recibí --> 12345 <-- por URL' }
+ */
+app.get('/api/:text', (req, res) => {
+  const { text } = req.params;
+  res.json({
+    result: 'ok',
+    msg:`Funciona!! recibí --> ${text} <-- por URL`
+  })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
